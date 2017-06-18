1ed3c772cf75ca145be05bc535df47c058377482
Manoj Khande <manoj.31khande@gmail.com>
2017-06-18 20:57:13 +0530

    Added first test

---
+++ tests/test-wordcamp.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wordcamp
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public $DefaultContent;
+
+	public function setUp()
+    {
+        $this->DefaultContent = new DefaultContent();
+    }
+ 
+    public function tearDown()
+    {
+        $this->DefaultContent = NULL;
+
+    }
+
+	public function test_init() {
+		$this->DefaultContent->init();
+	}
+
+	public function test_register_book_post_type(){
+		$this->DefaultContent->register_book_post_type();
+		$this->assertTrue( post_type_exists( 'book' ) );
+	}
+
+}
