e3a326d351033a2f20cc35f9e428da509ee6afa4
manojkhande <manoj.31khande@gmail.com>
2017-06-21 14:26:54 +0530

    Added Ajax tests

--- tests/test-wordcamp.php
+++ tests/test-wordcamp.php
@@ -41,3 +41,62 @@ class SampleTest extends WP_UnitTestCase {
 	}
 
 }
+
+/**
+ * Test case for the Ajax callback to update 'some_option'.
+ *
+ * @group ajax
+ */
+class My_Some_Option_Ajax_Test extends WP_Ajax_UnitTestCase {
+ 
+    /**
+     * Test that the callback saves the value for administrators.
+     */
+    public function test_some_option_is_saved() {
+ 
+        $this->_setRole( 'administrator' );
+ 
+        $_POST['_wpnonce'] = wp_create_nonce( 'my_nonce' );
+        $_POST['option_value'] = 'yes';
+ 
+        try {
+            $this->_handleAjax( 'my_ajax_action' );
+        } catch ( WPAjaxDieStopException $e ) {
+            // We expected this, do nothing.
+        }
+ 
+        // Check that the exception was thrown.
+        $this->assertTrue( isset( $e ) );
+ 
+        // The output should be a 1 for success.
+        $this->assertEquals( '1', $e->getMessage() );
+ 
+        $this->assertEquals( 'yes', get_option( 'some_option' ) );
+    }
+ 
+    /**
+     * Test that it doesn't work for subscribers.
+     */
+    public function test_requires_admin_permissions() {
+ 
+        $this->_setRole( 'subscriber' );
+ 
+        $_POST['_wpnonce'] = wp_create_nonce( 'my_nonce' );
+        $_POST['option_value'] = 'yes';
+ 
+        try {
+            $this->_handleAjax( 'my_ajax_action' );
+        } catch ( WPAjaxDieStopException $e ) {
+            // We expected this, do nothing.
+        }
+ 
+        // Check that the exception was thrown.
+        $this->assertTrue( isset( $e ) );
+ 
+        // The output should be a -1 for failure.
+        $this->assertEquals( '-1', $e->getMessage() );
+ 
+        // The option should not have been saved.
+        $this->assertFalse( get_option( 'some_option' ) );
+    }
+}
--- wordcamp.php
+++ wordcamp.php
@@ -17,6 +17,7 @@ class DefaultContent{
     {
     	add_action( 'init', array($this,'register_book_post_type') );
     	add_filter('the_content', array($this, 'wordcamp_filter_content'));
+    	add_action( 'wp_ajax_my_ajax_action', array($this,'my_some_option_ajax_callback') );
     }
 
     function register_book_post_type() {
@@ -31,6 +32,22 @@ class DefaultContent{
 		$new_content = '<p>This is added to the bottom of all post and page content.</p>';
 		$content .= $new_content;	
 		return $content;
+	}
+
+	/**
+	 * Ajax callback to save the 'some_option' option.
+	 */
+	function my_some_option_ajax_callback() {
+	 
+	    check_ajax_referer( 'my_nonce' );
+	 
+	    if ( ! isset( $_POST['option_value'] ) || ! current_user_can( 'manage_options' ) ) {
+	        wp_die( '-1' );
+	    }
+	 
+	    update_option( 'some_option', $_POST['option_value'] );
+	 
+	    wp_die( '1' );
 	}
 	
 }
